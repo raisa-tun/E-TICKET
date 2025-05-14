78f5521e35a4bb1c6fc219fd201032502f4e08a6
Raisa <raisa1995tabassum@gmail.com>
2025-05-14 14:26:59 +0600

    change the jquery version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,8 +29,9 @@
     <link rel="stylesheet" href="assets\css\ionicons.min.css">
     <!-- Main Style css -->
     <link rel="stylesheet" type="text/css" href="assets\css\style.css" media="all">
-      <!-- Jquery and Js Plugins -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <!-- Jquery and Js Plugins 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>-->
+    <script src="assets\js\jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="assets\js\bootstrap.min.js"></script>
     <script type="text/javascript" src="assets\js\plugins.js"></script>
     <script type="text/javascript" src="assets\js\menu.js"></script>
